770d2df07ed89aed2d7dcde5b83aad335a280fdb
Mengqi Yu <mengqiy@google.com>
2021-01-11 16:54:35 -0800

    move the logic of installing go-licenses to a script

---
+++ scripts/install-go-licenses.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+set -euo pipefail
+
+GO_LICENSES_TMP_DIR=$(mktemp -d)
+cd $GO_LICENSES_TMP_DIR
+go mod init tmp
+go get github.com/google/go-licenses
+export PATH=$PATH:$GOPATH/bin
+rm -rf $GO_LICENSES_TMP_DIR
