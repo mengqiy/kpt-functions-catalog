e5f351bf23072c9e7e8e29deb15e6ebd18a6d246
Mengqi Yu <mengqiy@google.com>
2021-01-11 18:29:05 -0800

    add quote

--- scripts/install-go-licenses.sh
+++ scripts/install-go-licenses.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 GO_LICENSES_TMP_DIR=$(mktemp -d)
-cd $GO_LICENSES_TMP_DIR
+cd "$GO_LICENSES_TMP_DIR"
 go mod init tmp
 go get github.com/google/go-licenses
 export PATH=$PATH:$GOPATH/bin
-rm -rf $GO_LICENSES_TMP_DIR
+rm -rf "$GO_LICENSES_TMP_DIR"
